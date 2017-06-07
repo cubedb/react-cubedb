f4156603e3f42fc7aa5a8a712f25347bce0d71ae
joao.ferreira <joao.ferreira@corp.badoo.com>
2017-06-07 14:04:52 +0100

    First commit, Boilerplate

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// Demo component
+// this is only example component
+// you can find tests in __test__ folder
+
+import React from 'react';
+
+class Cube extends React.Component {
+    componentDidMount() {
+        // some logic here - we only test if the method is called
+    }
+    render() {
+        return (
+            <div>Ta-da!</div>
+        )
+    }
+};
+
+export default Cube;
